e06c3c3be5d95b30334449f8e144428a7a2c7b76
Rubeen <rubeen@outlook.de>
2017-06-05 12:18:04 +0200

    added buttons to mainMenu, code for buttonPressed handling

---
+++ app/src/main/java/de/rubeen/android/ils/ils/UI/MainActivity.java
@@ -0,0 +1,43 @@
+package de.rubeen.android.ils.ils.UI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import de.rubeen.android.ils.ils.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button[] buttons = {(Button) findViewById(R.id.menu_btn_help),
+                (Button) findViewById(R.id.menu_btn_manual),
+                (Button) findViewById(R.id.menu_btn_settings),
+                (Button) findViewById(R.id.menu_btn_start)};
+        for (Button b : buttons)
+            b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.menu_btn_start:
+                System.out.println("pressed start");
+                break;
+            case R.id.menu_btn_settings:
+                System.out.println("pressed settings");
+                break;
+            case R.id.menu_btn_manual:
+                System.out.println("pressed manual");
+                break;
+            case R.id.menu_btn_help:
+                System.out.println("pressed help");
+                break;
+            default:
+                System.err.println("Button nicht gefunden:" + v.toString());
+        }
+    }
+}
